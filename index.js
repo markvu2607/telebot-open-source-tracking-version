c9c1d10e5d2f37bcf06fe42aff5cb6bf6320994c
Mark Vu <markvu.work@gmail.com>
2024-12-12 21:58:09 +0700

    refactor: remove vercel cronjob & tracking command

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import trackingVersion from "./lib/tracking-version.js";
-import { connectDB } from "./lib/db.js";
+import express from "express";
 import bot from "./lib/bot.js";
+import { connectDB } from "./lib/db.js";
+import { Following } from "./lib/schemas/following-schema.js";
+import { Package } from "./lib/schemas/package-schema.js";
+import { formatNewVersionResponse, getRepoInfo } from "./lib/utils.js";
 
 dotenv.config();
 await connectDB();
@@ -15,10 +17,48 @@ app.use(express.json());
 
 app.get("/api/tracking", async (req, res) => {
   try {
-    await trackingVersion();
+    // Get all packages
+    const oldPackages = await Package.find({});
+
+    // Get latest version of packages
+    const newPackages = await Promise.all(oldPackages.map(async (pkg) => {
+      // TODO: handle short description
+      const { version, description } = await getRepoInfo(pkg.name);
+      if (version === 'unknown') {
+        return {
+          name: pkg.name,
+          prevVersion: pkg.version,
+          newVersion: "unknown",
+          description: pkg.description,
+          status: 'Tracking failed'
+        }
+      }else {
+        await Package.updateOne({ name: pkg.name }, { version, description: "" });
+        return {
+          name: pkg.name,
+          prevVersion: pkg.version,
+          newVersion: version,
+          description: "",
+          status: 'Tracking success'
+        }
+      }
+    }));
+
+    // Filter packages that have new version
+    const packagesHasNewVersion = newPackages
+      .filter(pkg => pkg.newVersion !== "unknown")
+      .filter(pkg => pkg.newVersion !== pkg.prevVersion);
+
+    // Send message to all users who follow the package
+    await Promise.all(packagesHasNewVersion.map(async (pkg) => {
+      const following = await Following.find({ packageName: pkg.name });
+      await Promise.all(following.map(async ({chatId}) => {
+        await bot.telegram.sendMessage(chatId, formatNewVersionResponse(pkg));
+      }));
+    }));
+
     res.status(200).json({ message: 'Tracking completed' });
   } catch (error) {
-    console.error('Tracking error:', error);
     res.status(500).json({ error: 'Tracking failed' });
   }
 });
