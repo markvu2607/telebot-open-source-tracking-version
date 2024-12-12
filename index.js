55794ef8c81dbc93b4a05a3fad3cacc7010ebe36
Mark Vu <markvu.work@gmail.com>
2024-12-12 12:30:06 +0700

    feat: update tele lib and test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import { Telegraf } from "telegraf";
 dotenv.config();
 const port = process.env.PORT || 9999;
 
@@ -9,10 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.on("message", (ctx) => {
+  ctx.reply(
+    `${ctx.message.from.username} đã gửi: ${ctx.message.text} in ${ctx.message.chat.id} in ${ctx.message.chat.type}`
+  );
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+bot.startWebhook("/TrackingOpenSourceVersionBot", null, 3000);
