085863acbb4f282536ef564deb11c8562a76e7f4
Mark Vu <markvu.work@gmail.com>
2024-12-12 20:09:55 +0700

    refactor: update folder structure

--- index.js
+++ index.js
@@ -1,104 +1,31 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { Telegraf } from "telegraf";
-import { formatListResponse, getRepoInfo } from "./lib/utils.js";
 import trackingVersion from "./lib/tracking-version.js";
-import { Package } from "./lib/schemas/package-schema.js";
-import { Following } from "./lib/schemas/following-schema.js";
 import { connectDB } from "./lib/db.js";
+import bot from "./lib/bot.js";
 
 dotenv.config();
 await connectDB();
 const port = process.env.PORT || 9999;
 
-const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const listCommands = [
-  { command: "help", description: "See the list of commands" },
-  { command: "list", description: "See the list of following packages" },
-  { command: "add", description: "Add a package" },
-  { command: "remove", description: "Remove a package" },
-  { command: "tracking", description: "Tracking all packages" },
-];
-
-bot.start((ctx) => {
-  ctx.reply(`Hello! 👋. I'm a bot that can help you track open source version.\nYou can send me a message with the format:\n${listCommands.map((command, index) => `${index + 1}. /${command.command} - ${command.description}`).join("\n")}`);
+app.get("/api/tracking", async (req, res) => {
+  try {
+    await trackingVersion();
+    res.status(200).json({ message: 'Tracking completed' });
+  } catch (error) {
+    console.error('Tracking error:', error);
+    res.status(500).json({ error: 'Tracking failed' });
+  }
 });
 
-bot.help((ctx) => {
-  ctx.reply(`You can send me a message with the format:\n${listCommands.map((command, index) => `${index + 1}. /${command.command} - ${command.description}`).join("\n")}`);
+app.use(bot.webhookCallback("/bot"));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
-bot.command("list", async (ctx) => {
-  const username = ctx.message.from.username;
-  const chatId = ctx.message.chat.id;
-
-  const following = await Following.find({ chatId });
-  if (following.length === 0) {
-    ctx.reply(`User ${username} are not following any packages`);
-    return;
-  }
-
-  const packages = await Package.find({ name: { $in: following.map(item => item.packageName) } });
-  ctx.reply(`List of following packages: \n\n${formatListResponse(packages)}`);
-});
-
-bot.command("add", async (ctx) => {
-  const username = ctx.message.from.username;
-  const chatId = ctx.message.chat.id;
-  const packageName = ctx.message.text.split(" ")[1];
-
-  if (!packageName) {
-    ctx.reply("Please provide a package name");
-    return;
-  }
-
-  const isFollowing = await Following.findOne({ packageName, chatId });
-  if (isFollowing) {
-    ctx.reply(`Package ${packageName} is already being followed by ${isFollowing.username}`);
-    return;
-  }
-
-  const hasPackage = await Package.findOne({ name: packageName });
-  if (!hasPackage) {
-    // TODO: handle short description
-    const { version, description } = await getRepoInfo(packageName);
-    if (!version || version === 'unknown') {
-      ctx.reply(`Package ${packageName} not found`);
-      return;
-    }
-
-    await Package.create({ name: packageName, version, description: "" });
-  }
-
-  await Following.create({ chatId, packageName });
-  ctx.reply(`Package ${packageName} added to following by ${username}`);
-});
-
-bot.command("remove", async (ctx) => {
-  const username = ctx.message.from.username;
-  const chatId = ctx.message.chat.id;
-  const packageName = ctx.message.text.split(" ")[1];
-
-  if (!packageName) {
-    ctx.reply("Please provide a package name");
-    return;
-  }
-
-  const isFollowing = await Following.findOne({ packageName, chatId });
-  if (!isFollowing) {
-    ctx.reply(`Package ${packageName} is not being followed`);
-    return;
-  }
-
-  await Following.deleteOne({ chatId, packageName });
-  ctx.reply(`Package ${packageName} removed from following by ${username}`);
-});
-
-bot.command("tracking", async (ctx) => {
-  await trackingVersion();
-  ctx.reply("Tracking all packages");
-});
-
-bot.startWebhook("/bot", null, port);
