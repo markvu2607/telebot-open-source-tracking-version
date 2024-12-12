9b5587e60d96802d180d86f63c44819419ddd2db
Mark Vu <markvu.work@gmail.com>
2024-12-12 13:12:25 +0700

    feat: implement db

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { Telegraf } from "telegraf";
+import db from "./lib/db.js";
+
 dotenv.config();
 const port = process.env.PORT || 9999;
 
@@ -13,42 +15,49 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.start((ctx) => {
   ctx.reply(`
-    Hello! 👋). I'm a bot that can help you track open source version.
+    Hello! 👋. I'm a bot that can help you track open source version.
+
     You can send me a message with the format:
-    <code>
-    /help to see the list of commands
-    /list to see the list of following packages
-    /add to add a package
-    /remove to remove a package
-    /tracking to tracking all packages
-    </code>
+      /help to see the list of commands
+      /list to see the list of following packages
+      /add to add a package
+      /remove to remove a package
+      /tracking to tracking all packages
     `);
 });
 
 bot.help((ctx) => {
   ctx.reply(`
-  I can help you with tracking open source version.
   You can send me a message with the format:
-  <code>
-  /help
-  /list
-  /add
-  /remove
-  /tracking
-  </code>
+    /help to see the list of commands
+    /list to see the list of following packages
+    /add to add a package
+    /remove to remove a package
+    /tracking to tracking all packages
   `);
 });
 
 bot.command("list", (ctx) => {
-  ctx.reply("List of packages");
+  db.all("SELECT * FROM following WHERE username = ?", [ctx.message.from.username], (err, rows) => {
+    if (err) {
+      ctx.reply("Error: " + err.message);
+    } else {
+        ctx.reply(`List of following packages:
+          ${formatListResponse(rows)}
+        `);
+      }
+    }
+  );
 });
 
 bot.command("add", (ctx) => {
-  ctx.reply("Add a package");
+  db.exec("INSERT INTO following (username, package_name) VALUES (?, ?)", [ctx.message.from.username, ctx.message.text]);
+  ctx.reply(`Package ${ctx.message.text} added to following`);
 });
 
 bot.command("remove", (ctx) => {
-  ctx.reply("Remove a package");
+  db.exec("DELETE FROM following WHERE username = ? AND package_name = ?", [ctx.message.from.username, ctx.message.text]);
+  ctx.reply(`Package ${ctx.message.text} removed from following`);
 });
 
 bot.command("tracking", (ctx) => {
